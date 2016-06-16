3186f6ff009aa963b6ceb7841e1fb8ec4cb98493
Shuai Lin <linshuai2012@gmail.com>
2016-06-16 10:55:27 +0800

    Start the daemon in foregroud mode.

--- src/daemon-mgr.cpp
+++ src/daemon-mgr.cpp
@@ -60,7 +60,7 @@ void DaemonManager::startSeadriveDaemon()
     connect(seadrive_daemon_, SIGNAL(started()), this, SLOT(onDaemonStarted()));
 
     QStringList args;
-    args << "-d" << data_dir.absolutePath();
+    args << "-f" << "-d" << data_dir.absolutePath();
     args << "-l" << QDir(gui->logsDir()).absoluteFilePath("seadrive.log");
 
     QString fuse_opts = qgetenv("SEADRIVE_FUSE_OPTS");
