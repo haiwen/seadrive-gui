dbd752ab628efec4ab18d2024360846419661e80
Shuai Lin <linshuai2012@gmail.com>
2016-08-09 11:29:50 +0800

    Fixed extension/build.sh

--- extensions/build.sh
+++ extensions/build.sh
@@ -9,10 +9,10 @@ SRCDIR=$(dirname "${SCRIPT}")
 
 cd $SRCDIR
 
-# rm -rf CMakeCache.txt CMakeFiles
-# export CXX=g++
-# cmake -DX32=ON -G "MSYS Makefiles" .
-# make
+rm -rf CMakeCache.txt CMakeFiles
+export CXX=g++
+cmake -DX32=ON -G "MSYS Makefiles" .
+make
 
 rm -rf CMakeCache.txt CMakeFiles
 export CXX=x86_64-w64-mingw32-g++
