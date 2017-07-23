13d362c3432c61f1ee249c21ad8aad17285bf0dc
Shuai Lin <linshuai2012@gmail.com>
2017-07-23 14:35:53 +0800

    Added compatible maros to build with old libjansson.

--- src/utils/json-utils.h
+++ src/utils/json-utils.h
@@ -17,4 +17,28 @@ private:
     const json_t *json_;
 };
 
+
+#if !defined(json_object_foreach)
+#define json_object_foreach(object, key, value) \
+    for(key = json_object_iter_key(json_object_iter(object)); \
+        key && (value = json_object_iter_value(json_object_key_to_iter(key))); \
+        key = json_object_iter_key(json_object_iter_next(object, json_object_key_to_iter(key))))
+#endif
+
+#if !defined(json_object_foreach_safe)
+#define json_object_foreach_safe(object, n, key, value)     \
+    for(key = json_object_iter_key(json_object_iter(object)), \
+            n = json_object_iter_next(object, json_object_key_to_iter(key)); \
+        key && (value = json_object_iter_value(json_object_key_to_iter(key))); \
+        key = json_object_iter_key(n), \
+            n = json_object_iter_next(object, json_object_key_to_iter(key)))
+#endif
+
+#if !defined(json_object_foreach)
+#define json_array_foreach(array, index, value) \
+	for(index = 0; \
+		index < json_array_size(array) && (value = json_array_get(array, index)); \
+		index++)
+#endif
+
 #endif  // SEAFILE_CLIENT_UTILS_JSON_UTILS_H
