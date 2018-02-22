230af69c047b830ada5b1e8625b7a76ba28d8351
Shuai Lin <linshuai2012@gmail.com>
2018-02-22 10:59:49 +0800

    osx helper: add a build script

---
+++ osxhelper/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -x -e
+
+CURRENT_PWD="$(dirname "${BASH_SOURCE[0]}")"
+
+pushd $CURRENT_PWD
+rm -rf CMakeCache.txt CMakeFiles
+cmake -GNinja -DCMAKE_BUILD_TYPE="Release"
+ninja
+
+if [[ $# -gt 0 ]]; then
+    appdir=$1
+    servicedir=${appdir}/Contents/Library/LaunchServices
+    mkdir -p $servicedir
+    cp -v seadrive-helper $servicedir
+fi
