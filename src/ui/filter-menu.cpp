c4932d8acc8b8331f2e0637f26962a07971a4e68
sunyuzhao <32261840+sunyuzhao@users.noreply.github.com>
2018-03-27 11:57:51 +0800

    Adjust the load-more-button (#59)
    * Support search files
    
    * Adjust the search view
    
    * Adjust show in explorer/finder
    
    * Adjust code
    
    * Add the 'loading more' function
    
    * Add the filter function in the search-dialog
    
    * Add Empty View and Loading-View
    
    * Display Repo-name in the search-table
    
    * Fix bug after rebase ’loading more‘
    
    * Remove the input filter
    Document contains pdf
    
    * fix 'Document contains pdf'
    
    * Fix support-search-function-rebase-loading
    
    * Adjust the load-more-button

---
+++ src/ui/filter-menu.cpp
@@ -0,0 +1,80 @@
+#include "filter-menu.h"
+
+
+namespace
+{
+const QString kFileTypeTextFile = "Text";
+const QString kFileTypeDocument = "Document";
+const QString kFileTypeImage = "Image";
+const QString kFileTypeVideo = "Video";
+const QString kFileTypeAudio = "Audio";
+const QString kFileTypePdf = "PDF";
+const QString kFileTypeMarkdown = "Markdown";
+
+} // namespace
+
+FilterMenu::FilterMenu(QWidget *parent)
+    : QWidget(parent),
+      last_filters_(QStringList())
+{
+    setupUi(this);
+    setStyleSheet("QWidget#mFilter {"
+                      "border-bottom : 1px solid #d0d0d0;}");
+    connect(mTextFile, SIGNAL(clicked(bool)),
+            this, SLOT(onBoxChanged()));
+    connect(mDocument, SIGNAL(clicked(bool)),
+            this, SLOT(onBoxChanged()));
+    connect(mImage, SIGNAL(clicked(bool)),
+            this, SLOT(onBoxChanged()));
+    connect(mVideo, SIGNAL(clicked(bool)),
+            this, SLOT(onBoxChanged()));
+    connect(mAudio, SIGNAL(clicked(bool)),
+            this, SLOT(onBoxChanged()));
+    connect(mMarkdown, SIGNAL(clicked(bool)),
+            this, SLOT(onBoxChanged()));
+}
+
+void FilterMenu::onBoxChanged()
+{
+    QStringList new_filters = filterList();
+    if (!new_filters.isEmpty()) {
+        if (new_filters != last_filters_) {
+            last_filters_ = new_filters;
+            emit filterChanged();
+        }
+    }
+}
+
+void FilterMenu::clearCheckBox()
+{
+    mTextFile->setChecked(false);
+    mDocument->setChecked(false);
+    mImage->setChecked(false);
+    mVideo->setChecked(false);
+    mAudio->setChecked(false);
+    mMarkdown->setChecked(false);
+}
+
+QStringList FilterMenu::filterList() {
+    QStringList types;
+    if (mTextFile->isChecked()) {
+        types.append(kFileTypeTextFile);
+    }
+    if (mDocument->isChecked()) {
+        types.append(kFileTypeDocument);
+        types.append(kFileTypePdf);
+    }
+    if (mImage->isChecked()) {
+        types.append(kFileTypeImage);
+    }
+    if (mVideo->isChecked()) {
+        types.append(kFileTypeVideo);
+    }
+    if (mAudio->isChecked()) {
+        types.append(kFileTypeAudio);
+    }
+    if (mMarkdown->isChecked()) {
+        types.append(kFileTypeMarkdown);
+    }
+    return types;
+}
---
+++ src/ui/search-dialog.h
@@ -0,0 +1,170 @@
+#ifndef SEAFILE_CLIENT_SEARCH_DIALOG_H
+#define SEAFILE_CLIENT_SEARCH_DIALOG_H
+#include <QDialog>
+#include <QStyledItemDelegate>
+#include <QTableView>
+#include <QTableWidgetItem>
+#include <QTimer>
+#include <vector>
+
+#include "search-bar.h"
+#include "api/requests.h"
+#include "account.h"
+#include "ui/filter-menu.h"
+
+class ApiError;
+class QToolBar;
+class QStackedWidget;
+class QLabel;
+class LoadMoreButton;
+class QRadioButton;
+class QCheckBox;
+class SearchBar;
+class FilterMenu;
+class SearchItemsTableView;
+class SearchItemsTableModel;
+class SearchItemsDelegate;
+struct FileSearchResult;
+class FileSearchRequest;
+
+class SearchDialog : public QDialog
+{
+    Q_OBJECT
+public:
+    SearchDialog(const Account &account, QWidget *parent = 0);
+    ~SearchDialog();
+signals:
+    void aboutClose();
+private slots:
+    void openFilterMenu();
+    void closeFilterMenu(bool checked);
+    void onRefresh(bool loading_more = false);
+    void doSearch(const QString& keyword);
+    void doRealSearch(bool load_more = false,
+                      bool isAll = true,
+                      const QStringList& filter_list = QStringList());
+    void onSearchSuccess(const std::vector<FileSearchResult>& results,
+                         bool is_loading_more,
+                         bool has_more);
+    void onSearchFailed(const ApiError& error);
+    void loadMoreSearchResults();
+
+private:
+    void closeEvent(QCloseEvent *ev);
+    void createToolBar();
+    void createFilterMenu();
+    void createLoadingView();
+    void createEmptyView();
+    void createTable();
+
+    bool eventFilter(QObject *obj, QEvent *event);
+
+    const Account account_;
+
+    QTimer *search_timer_;
+    FileSearchRequest *search_request_;
+    qint64 search_text_last_modified_;
+
+    //toolbar
+    QToolBar *toolbar_;
+//    QToolButton *refresh_button_;
+    QRadioButton *search_all_file_;
+    QRadioButton *search_custom_file_;
+
+    //menu
+    FilterMenu *filter_menu_;
+
+    //stack
+    QStackedWidget *stack_;
+    QLabel *loading_failed_view_;
+    QWidget *waiting_view_;
+    QWidget *loading_view_;
+    QLabel *empty_view_;
+
+    LoadMoreButton *load_more_btn_;
+    SearchBar *search_bar_;
+    SearchItemsTableView* search_view_;
+    SearchItemsTableModel* search_model_;
+    SearchItemsDelegate* search_delegate_;
+
+    int nth_page_;
+    int loading_row_;
+};
+
+class SearchItemsTableView : public QTableView
+{
+    Q_OBJECT
+public:
+    SearchItemsTableView(QWidget* parent = 0);
+    void resizeEvent(QResizeEvent* event) Q_DECL_OVERRIDE;
+    void setModel(QAbstractItemModel* model) Q_DECL_OVERRIDE;
+//signals:
+//    void clearSearchBar();
+private slots:
+    void onAboutToReset();
+    void onItemDoubleClick(const QModelIndex& index);
+
+private:
+
+    QScopedPointer<const FileSearchResult> search_item_;
+    SearchDialog *parent_;
+    // source model
+    SearchItemsTableModel *search_model_;
+};
+
+
+class SearchItemsTableModel : public QAbstractTableModel
+{
+    Q_OBJECT
+public:
+    SearchItemsTableModel(QObject* parent = 0);
+
+    int rowCount(const QModelIndex& parent = QModelIndex()) const
+        Q_DECL_OVERRIDE;
+    int columnCount(const QModelIndex& parent = QModelIndex()) const
+        Q_DECL_OVERRIDE;
+
+    const QTableWidgetItem* item(const QModelIndex& index) const
+    {
+        if (!index.isValid() || index.row() >= (int)items_.size())
+            return NULL;
+        return items_[index.row()];
+    }
+
+    QVariant data(const QModelIndex& index,
+                  int role = Qt::DisplayRole) const Q_DECL_OVERRIDE;
+    QVariant headerData(int section,
+                        Qt::Orientation orientation,
+                        int role) const Q_DECL_OVERRIDE;
+    const QModelIndex updateSearchResults(const std::vector<QTableWidgetItem *> &items, bool is_loading_more, bool has_more);
+    const QModelIndex loadMoreIndex() const { return load_more_index_; }
+
+    void onResize(const QSize &size);
+    const FileSearchResult* resultAt(int row) const;
+
+    void clear()
+    {
+        for (unsigned i = 0 ; i < items_.size(); ++i) {
+            delete items_[i];
+        }
+        items_.clear();
+    }
+
+private:
+    std::vector<QTableWidgetItem*> items_;
+
+    int name_column_width_;
+    QModelIndex load_more_index_;
+};
+
+class DataManager;
+class ThumbnailService;
+class SearchItemsDelegate : public QStyledItemDelegate {
+    Q_OBJECT
+public:
+    SearchItemsDelegate(QObject *parent);
+    void paint(QPainter *painter,
+               const QStyleOptionViewItem &option,
+               const QModelIndex &index) const;
+};
+#endif //SEAFILE_CLIENT_SEARCH_DIALOG_H
