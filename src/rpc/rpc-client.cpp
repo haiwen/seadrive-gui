4fbba82f033be3521311ebe10c0614aca2a035dd
YuZhao Sun <yuzhao.sun@seafile.com>
2018-08-23 11:45:32 +0800

    Support pause sync.

--- src/rpc/rpc-client.cpp
+++ src/rpc/rpc-client.cpp
@@ -684,3 +684,25 @@ bool SeafileRpcClient::cachePath(const QString& repo_id,
         return true;
     }
 }
+
+int SeafileRpcClient::setAutoSync(bool autoSync)
+{
+    GError *error = NULL;
+    int ret;
+    if (autoSync) {
+        ret = searpc_client_call__int (seadrive_rpc_client_,
+                                       "seafile_enable_auto_sync",
+                                       &error, 0);
+    } else {
+        ret = searpc_client_call__int (seadrive_rpc_client_,
+                                       "seafile_disable_auto_sync",
+                                       &error, 0);
+    }
+
+    if (error) {
+        qWarning("failed to set auto_sync: %s\n", error->message);
+        g_error_free(error);
+    }
+
+    return ret;
+}
