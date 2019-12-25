7c1358c7a615611990eb951f5fc7871a0db95220
Shuai Lin <linshuai2012@gmail.com>
2019-12-25 11:11:52 +0800

    Removed unused code

--- src/application.cpp
+++ src/application.cpp
@@ -26,16 +26,16 @@ static bool dockClickHandler(id self,SEL _cmd,...)
 
 Application::Application (int &argc, char **argv):QApplication(argc, argv)
 {
-    objc_object* cls = (objc_object *)objc_getClass("NSApplication");
-    SEL sharedApplication = sel_registerName("sharedApplication");
-    objc_object* appInst = objc_msgSend(cls,sharedApplication);
+    // objc_object* cls = (objc_object *)objc_getClass("NSApplication");
+    // SEL sharedApplication = sel_registerName("sharedApplication");
+    // objc_object* appInst = objc_msgSend(cls,sharedApplication);
 
-    if(appInst != NULL)
-    {
-        objc_object* delegate = objc_msgSend(appInst, sel_registerName("delegate"));
-        objc_object* delClass = objc_msgSend(delegate,  sel_registerName("class"));
-        class_addMethod((Class)delClass, sel_registerName("applicationShouldHandleReopen:hasVisibleWindows:"), (IMP)dockClickHandler, "B@:");
-    }
+    // if(appInst != NULL)
+    // {
+    //     objc_object* delegate = objc_msgSend(appInst, sel_registerName("delegate"));
+    //     objc_object* delClass = objc_msgSend(delegate,  sel_registerName("class"));
+    //     class_addMethod((Class)delClass, sel_registerName("applicationShouldHandleReopen:hasVisibleWindows:"), (IMP)dockClickHandler, "B@:");
+    // }
 }
 
 bool Application::event(QEvent *e)
