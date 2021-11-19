cbf4f4cd2bfbe4e3b5ead55fa820ec66389833ee
sun20121818 <1184550842@qq.com>
2021-11-19 11:42:25 +0800

    [Windows]Delete syncroot when the user confirms the deletion of user data when uninstalling the program

--- src/utils/uninstall-helpers.cpp
+++ src/utils/uninstall-helpers.cpp
@@ -124,44 +124,19 @@ int posix_rmdir(const QString &root)
 int delete_dir_recursively(const QString& path_in)
 {
     qWarning ("removing folder %s\n", toCStr(path_in));
-#if defined(Q_OS_WIN32)
-    const QString path = QDir::toNativeSeparators(QDir::cleanPath(path_in));
-    if (path.length() <= 3) {
+    if (path_in.length() <= 3) {
         // avoid errornous delete drives like C:/ D:/ E:/
         return -1;
     }
 
-    int len = path.length();
-
-    wchar_t *wpath = new wchar_t[len + 2];
-
-    wcscpy(wpath, path.toStdWString().c_str());
-    wpath[len + 1] = L'\0';
-
-    SHFILEOPSTRUCTW fileop;
-    fileop.hwnd   = NULL;       // no status display
-    fileop.wFunc  = FO_DELETE;  // delete operation
-    fileop.pFrom  = wpath; // source file name as double null terminated string
-    fileop.pTo    = NULL;         // no destination needed
-    fileop.fFlags = FOF_NOCONFIRMATION|FOF_SILENT; // do not prompt the user
-
-    fileop.fAnyOperationsAborted = FALSE;
-    fileop.lpszProgressTitle     = NULL;
-    fileop.hNameMappings         = NULL;
-
-    int ret = SHFileOperationW(&fileop);
-
-    delete []wpath;
-
-    if (ret == 0) {
-        return 0;
-    } else {
-        return -1;
+    QDir dir (path_in);
+    bool ok = dir.removeRecursively();
+    if (!ok) {
+        qWarning("failed to removeRecursively remove directory %s",toCStr(path_in));
+        return -1 ;
     }
     return 0;
-#else
-    return posix_rmdir(path_in);
-#endif
+
 }
 
 void do_stop_app()
@@ -189,10 +164,6 @@ void do_remove_user_data()
 {
     do_stop_app();
     set_seafile_auto_start(false);
-
-#if defined(_MSC_VER)
-    RegElement::removeAllSyncRootManagerItem();
-#endif
 
 #if defined(Q_OS_WIN32)
     if (hasPreconfigureKeepConfigWhenUninstall()) {
@@ -283,14 +254,13 @@ static void read_account_info_from_db(std::vector<Account>* ptr_accounts)
     userdata.db = db;
     sqlite_foreach_selected_row (db, sql, loadAccountsCB, &userdata);
 
+    sqlite3_close(db);
     return;
 
 }
 
 void do_seadrive_unregister_sync_root()
 {
-
-    do_stop_app();
 
     std::vector<Account> accounts;
     read_account_info_from_db(&accounts);
