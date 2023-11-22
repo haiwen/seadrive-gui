798b0b7ba1057f006ef914fb90998bdcf1958fa8
feiniks <36756310+feiniks@users.noreply.github.com>
2023-11-22 11:07:52 +0800

    Notify folder of downloaded files (#388)
    * Notify folder of downloaded files
    
    * Modify notify
    
    ---------
    
    Co-authored-by: heran yang <heran.yang@seafile.com>

--- src/ui/tray-icon.cpp
+++ src/ui/tray-icon.cpp
@@ -660,7 +660,11 @@ void SeafileTrayIcon::deleteAccount()
         return;
     }
 
+#ifdef Q_OS_WIN32
     QString question = tr("Are you sure to remove account from \"%1\"?").arg(account.serverUrl.toString());
+#else
+    QString question = tr("Are you sure to remove account from \"%1\"? After removing account, you can still find downloaded files at ~/Library/CloudStorage.").arg(account.serverUrl.toString());
+#endif
 
     if (!gui->yesOrNoBox(question, nullptr, false)) {
         return;
