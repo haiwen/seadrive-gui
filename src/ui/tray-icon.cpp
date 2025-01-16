cd10edf34b39718e1663c56ece144d6fc63c90fe
Heran Yang <heran.yang@seafile.com>
2025-01-16 13:59:37 +0800

    Modify display_name and launch sync dialog

--- src/ui/tray-icon.cpp
+++ src/ui/tray-icon.cpp
@@ -717,7 +717,7 @@ void SeafileTrayIcon::deleteAccount()
 #endif
 
 #if defined(Q_OS_WIN32) || defined(Q_OS_LINUX)
-    QString question = tr("Are you sure to remove account from \"%1\"?").arg(account.serverUrl.toString());
+    QString question = tr("Are you sure to remove account from \"%1\"? Downloaded and uploading files will not be removed.").arg(account.serverUrl.toString());
 #else
     QString question = tr("Are you sure to remove account from \"%1\"? After removing account, you can still find downloaded files at ~/Library/CloudStorage.").arg(account.serverUrl.toString());
 #endif
